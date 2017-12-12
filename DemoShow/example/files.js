55d251ca14f89b28f7af3fbdf3914fbf8fefd1c0
StringKun <351281238@qq.com>
2017-12-12 08:50:10 +0800

    版本1
    添加了demoshow

---
+++ DemoShow/example/files.js
@@ -0,0 +1,20 @@
+var files = {
+	"webgl": [
+		"webgl-mirror",
+		"webgl-stroke",
+		"webgl-vao",
+        "webgl-shadow"
+
+	],
+	"threejs": [
+		"bursting",
+        "glow",
+        "potcloud1",
+        "potcloud2",
+        "potcloud3",
+        "panorama",
+        "toonshader1",
+        "toonshader2"
+	]
+
+};
